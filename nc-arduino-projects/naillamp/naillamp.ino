1bbf117413f0a41704374f303d6f82098d465ee4
Roman Horokhov <roman.horokhov@nextiva.com>
2018-06-03 16:31:45 +0300

    test

--- nc-arduino-projects/naillamp/naillamp.ino
+++ nc-arduino-projects/naillamp/naillamp.ino
@@ -1,3 +1,11 @@
+#include <FastLED.h>
+
+#include "FastLED_RGBW.h" // Include the FastLED_RGBW helper file
+
+#define LED_COUNT 60
+
+#define LED_OUT 6
+
 const int button = 5;
 const int maxBrightness = 255;
 const int minBrightness = 0;
@@ -6,12 +14,20 @@ int steps = 8;
 bool goingUp = true;
 bool changed = false;
 
-int ledPin = 6;
+CRGBW leds[LED_COUNT]; // FastLED with RGBW
+CRGB *ledsRGB = (CRGB *)&leds[0];
+
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  LEDS.addLeds<WS2812B, LED_OUT, RGB>(ledsRGB, getRGBWsize(LED_COUNT)); //use both the CRGB pointer and the size function "getRGBWsize" in the FastLED_RGBW helper file
+
+  //  brightness = STARTING_BRIGHTNESS;
+  //  LEDS.setBrightness(brightnesSteps[brightness]);
+  LEDS.setBrightness(brightness);
+  LEDS.show();
+  pinMode(LED_OUT, OUTPUT);
   pinMode(button, INPUT);
   initialFadeIn();
 }
@@ -22,6 +38,7 @@ void initialFadeIn()
   for (int i = 0; i < 255 / steps; i = i + steps)
   {
     changeBrightness(true);
+    LEDS.show();
     delay(100);
   }
   Serial.println("Finished initial fade-in.");
@@ -31,10 +48,11 @@ void changeBrightness(bool direction)
 {
   if (direction)
   {
-    if (brightness < 255)
+    if (brightness < 255 - steps)
     {
       Serial.println("Increasing brightness");
       brightness += steps;
+      LEDS.setBrightness(brightness);
     }
     else
     {
@@ -47,6 +65,7 @@ void changeBrightness(bool direction)
     {
       Serial.println("Decreasing brightness");
       brightness -= steps;
+      LEDS.setBrightness(brightness);
     }
     else
     {
@@ -80,6 +99,7 @@ void loop()
   Serial.print("Brightness: ");
   Serial.println(brightness);
   Serial.println("=============== FIN ===============");
-
+//  LEDS.show();
+  LEDS.show();
   delay(500);
 }
